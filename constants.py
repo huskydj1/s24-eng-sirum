1536fabcc47ba6df104b97ba07144e3f181d8989
Davin Jeong <husky.davin@gmail.com>
2024-05-07 15:01:31 -0400

    add chromedriver download link

--- constants.py
+++ constants.py
@@ -4,6 +4,6 @@
 SERPAPI_KEY = ""
 
 # Path to Chrome_Webdriver:
-# We use Selenium to automate browser interaction with Python. To do so, we must give it access to our chrome webdriver. You may link to your existing chrome driver, or download one at the following link: #TODO ADD LINK
+# We use Selenium to automate browser interaction with Python. To do so, we must give it access to our chrome webdriver. You may link to your existing chrome driver, or download one separately (https://googlechromelabs.github.io/chrome-for-testing/)
 
 CHROME_WEBDRIVER = ""
